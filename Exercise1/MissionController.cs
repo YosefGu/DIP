a8c68bd0f8ca8257b06d5662c73962392b7e996f
YosefGu <yos770yos@gmail.com>
2025-06-19 15:25:26 +0300

    step 1

---
+++ Exercise1/MissionController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIP.Exercise1
+{
+    internal class MissionController
+    {
+        public void RinMission()
+        {
+            string mission1 = "Running mission 1";
+            FileLogger logger = new FileLogger();
+            logger.Log($"Mission complete: {mission1}");
+        }
+    }
+}
